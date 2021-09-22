88eda51bcaba064e39e75537308dfe891f9a1f69
Surajwish <vishwakarmasuraj10@gmail.com>
2021-09-22 21:02:14 +0530

    modification
    adding smooth scroll
    and navigation modification

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const bar = document.getElementById('mobile_view');
+
 let menu = document.getElementById('menu_list');
+
+let home = document.getElementById('home-page');
+
+let service = document.getElementById('services-page');
+
+let me =document.getElementById('menu-page');
+
 function toggleview(){
-    menu.classList.toggle('active');
+
+  menu.classList.toggle('active');
+
 }
+
 bar.addEventListener('click',toggleview);
+
+home.addEventListener('click',toggleview);
+
+me.addEventListener('click',toggleview);
+
+service.addEventListener('click',toggleview);
